d1c3d14b4e86b4a50b9d96c818453a6375277ecb
BirajSingha <67847698+BirajSingha@users.noreply.github.com>
2024-07-16 14:23:36 +0530

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,23 @@
+export default {
+  //TOKENS
+  REG_TOKEN: 'REG_TOKEN',
+  TOKEN: 'TOKEN',
+  LOGIN_TOKEN: 'LOGIN_TOKEN',
+
+  //DEV URLS
+  BASE_URL: 'https://cargogo.dedicateddevelopers.us/api',
+  SOCKET_URL: 'https://cargogo.dedicateddevelopers.us',
+  PROFILE_IMAGE_URL:
+    'https://cargogo.dedicateddevelopers.us/uploads/user/profile_pic/',
+  CAR_IMAGE_URL: 'https://cargogo.dedicateddevelopers.us/uploads/vehicle/',
+  CAR_FILE_URL: 'https://cargogo.dedicateddevelopers.us/uploads/booking/',
+  CHAT_FILE_URL: 'https://cargogo.dedicateddevelopers.us/uploads/chat/',
+
+  //LOCAL URLS
+  // BASE_URL: 'http://192.168.8.41:1773/api',
+  // SOCKET_URL: 'http://192.168.8.41:1773',
+  // PROFILE_IMAGE_URL: 'http://192.168.8.41:1773/uploads/user/profile_pic/',
+  // CAR_IMAGE_URL: 'http://192.168.8.41:1773/uploads/vehicle/',
+  // CAR_FILE_URL: 'http://192.168.8.41:1773/uploads/booking/',
+  // CHAT_FILE_URL: 'http://192.168.8.41:1773/uploads/chat/',
+};
---
+++ utils/themes.js
@@ -0,0 +1,98 @@
+import {Dimensions} from 'react-native';
+
+//------------COLOR CONSTANTS------------//
+export const COLORS = {
+  black: '#000000',
+  lightBlack: '#545050',
+  white: '#ffffff',
+  offwhite: '#FEFEFE',
+  offWhiteBG: '#F8F8F8',
+  offGray: '#A8A8A8',
+  textInputBorder: '#F6F3F3',
+  placeholderTextInput: '#A3A3A3',
+  textInputColor: '#3E3838',
+  lightGreen: '#64F679',
+  cardBg: '#F1F3F5',
+  gold: '#B9A972',
+};
+
+//------------FONT CONSTANTS------------//
+export const FONTS = {
+  JAKARTA_SANS_BOLD: 'PlusJakartaSans-Bold',
+  JAKARTA_SANS_LIGHT: 'PlusJakartaSans-Light',
+  JAKARTA_SANS_MEDIUM: 'PlusJakartaSans-Medium',
+  JAKARTA_SANS_REGULAR: 'PlusJakartaSans-Regular',
+  JAKARTA_SANS_EXTRA_BOLD: 'PlusJakartaSans-ExtraBold',
+};
+
+//------------IMAGES CONSTANTS------------//
+export const IMAGES = {
+  splash: require('../assets/images/Splash.jpg'),
+  AuthBG: require('../assets/images/AuthBG.jpg'),
+  logo: require('../assets/images/Logo.png'),
+  darkLogo: require('../assets/images/DarkLogo.png'),
+  cardType: require('../assets/images/CardType.png'),
+  masterCard: require('../assets/images/mastercard.png'),
+  amex: require('../assets/images/amex.png'),
+  diners: require('../assets/images/diners.png'),
+  discover: require('../assets/images/discover.png'),
+  jcb: require('../assets/images/jcb.png'),
+  chip: require('../assets/images/Chip.png'),
+  carBG: require('../assets/images/Car.png'),
+  profileBG: require('../assets/images/ProfileBG.png'),
+  demoProfile: require('../assets/images/DemoProfile.png'),
+  supportBG: require('../assets/images/supportBG.png'),
+  previewImage: require('../assets/images/previewImage.png'),
+};
+
+//------------ICONS CONSTANTS------------//
+export const ICONS = {
+  email: require('../assets/icons/email.png'),
+  eyeOff: require('../assets/icons/eyeOff.png'),
+  eyeOn: require('../assets/icons/eyeOn.png'),
+  phone: require('../assets/icons/phone.png'),
+  home: require('../assets/icons/home.png'),
+  inbox: require('../assets/icons/inbox.png'),
+  trip: require('../assets/icons/trip.png'),
+  profile: require('../assets/icons/profile.png'),
+  check: require('../assets/icons/check.png'),
+  person: require('../assets/icons/person.png'),
+  calander: require('../assets/icons/calander.png'),
+  darkCalander: require('../assets/icons/darkCalander.png'),
+  clock: require('../assets/icons/clock.png'),
+  circledRightArrow: require('../assets/icons/circledRightArrow.png'),
+  suitcase: require('../assets/icons/suitcase.png'),
+  backIcon: require('../assets/icons/backIcon.png'),
+  fuel: require('../assets/icons/fuel.png'),
+  seat: require('../assets/icons/seat.png'),
+  tickFilled: require('../assets/icons/tickFilled.png'),
+  tickOutlined: require('../assets/icons/tickOutlined.png'),
+  downArrow: require('../assets/icons/downArrow.png'),
+  upArrow: require('../assets/icons/upArrow.png'),
+  rightArrow: require('../assets/icons/rightArrow.png'),
+  arrowSeperator: require('../assets/icons/arrowSeperator.png'),
+  delete: require('../assets/icons/delete.png'),
+  personalCard: require('../assets/icons/personalcard.png'),
+  receipt: require('../assets/icons/receipt.png'),
+  card: require('../assets/icons/card.png'),
+  cards: require('../assets/icons/cards.png'),
+  promo: require('../assets/icons/promo.png'),
+  documentCloud: require('../assets/icons/documentCloud.png'),
+  tip: require('../assets/icons/tip.png'),
+  circledCross: require('../assets/icons/circledCross.png'),
+  roundTick: require('../assets/icons/roundTick.png'),
+  camera: require('../assets/icons/camera.png'),
+  editProfile: require('../assets/icons/editProfile.png'),
+  support: require('../assets/icons/support.png'),
+  infoCircle: require('../assets/icons/infoCircle.png'),
+  logout: require('../assets/icons/logout.png'),
+  attachment: require('../assets/icons/attachment.png'),
+  sendIcon: require('../assets/icons/sendIcon.png'),
+  city: require('../assets/icons/city.png'),
+  globe: require('../assets/icons/globe.png'),
+};
+
+export const SIZES = {
+  width: Dimensions.get('window').width,
+  height: Dimensions.get('window').height,
+};
