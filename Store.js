c2f692cfff5e390e62cbb50bcff89f346c308259
BirajSingha <67847698+BirajSingha@users.noreply.github.com>
2024-07-16 14:04:00 +0530

    Update Store.js

--- Store.js
+++ Store.js
@@ -4,8 +4,6 @@ import {logger} from 'redux-logger';
 import AuthReducer from './reducers/AuthReducer';
 import RootSaga from './saga/RootSaga';
 import UserReducer from './reducers/UserReducer';
-import VehicleReducer from './reducers/VehicleReducer';
-import BookingReducer from './reducers/BookingReducer';
 import ChatReducer from './reducers/ChatReducer';
 
 let sagaMiddleware = createSagaMiddleware();
@@ -15,8 +13,6 @@ export default configureStore({
   reducer: {
     AuthReducer: AuthReducer,
     UserReducer: UserReducer,
-    VehicleReducer: VehicleReducer,
-    BookingReducer: BookingReducer,
     ChatReducer: ChatReducer,
   },
   middleware: getDefaultMiddleware => {
